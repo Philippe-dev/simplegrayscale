6ee329446e4732dc1e25c4693b18e50df09a2c4c
padawan83 <philippe@dissitou.org>
2020-03-04 15:32:33 +0100

    initial commit

---
+++ _define.php
@@ -0,0 +1,23 @@
+<?php
+# -- BEGIN LICENSE BLOCK ---------------------------------------
+# This file is part of Grayscale, a theme for Dotclear
+#
+# Copyright (c) 2015 - Association Dotclear
+# Licensed under the GPL version 2.0 license.
+# See LICENSE file or
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK -----------------------------------------
+
+if (!defined('DC_RC_PATH')) { return; }
+
+$this->registerModule(
+	/* Name */				"Simple Grayscale",
+	/* Description*/		"Simple Grayscale Bootstrap theme for Dotclear",
+	/* Author */			"Start Bootstrap and Philippe aka amalgame",
+	/* Version */			'1.6.0',
+	/* Properties */		array(
+								'type' => 'theme',
+								'tplset' => 'mustek'
+							)
+);
