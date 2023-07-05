362536aa335491683081dac3e8d4907df2e4d461
“philippe <philippe@dissitou.org>
2023-07-05 09:55:52 +0200

    Code review

--- src/Config.php
+++ src/Config.php
@@ -109,7 +109,7 @@ class Config extends Process
 
         dcCore::app()->admin->conf_tab = $_POST['conf_tab'] ?? 'images';
 
-        return self::$init;
+        return self::status();
     }
 
     /**
