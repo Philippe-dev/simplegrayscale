a4e9417fde6c58d80b355f12c62ed84bfb307147
“philippe <philippe.mail.perso@gmail.com>
2024-12-20 09:15:19 +0100

    fix link on tag page

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Simple Grayscale',
     'Simple Grayscale Bootstrap 5 theme for Dotclear',
     'Philippe aka amalgame and contributors',
-    '4.3',
+    '4.4',
     [
         'requires'          => [['core', '2.28']],
         'standalone_config' => true,
